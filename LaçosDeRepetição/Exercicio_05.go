f1cdc6a029da35497a3b1478fae02b8ed0d0e592
Tais <taisfilippe@gmail.com>
2021-07-27 20:28:52 -0300

    add item 1

--- LaçosDeRepetição/Exercicio_05.go
+++ LaçosDeRepetição/Exercicio_05.go
@@ -3,6 +3,32 @@ package main
 // Faça um programa que, dado um texto inserido pelo usuário, itere nesse texto e conte o número de ocorrências de cada letra.
 // Em seguida imprima em ordem alfabética a letra e o número de ocorrências dela no texto informado.
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	frase := leiaLinha()
+	ocorrencias := make(map[byte]int)
+
+	for i := 0; i < len(frase); i++ {
+
+		quantidade, existe := ocorrencias[frase[i]]
+		if existe {
+			ocorrencias[frase[i]] = quantidade + 1
+		} else {
+			ocorrencias[frase[i]] = 1
+		}
+	}
+
+	for letra, valor := range ocorrencias {
+		fmt.Printf("%c: %v \n", letra, valor)
+	}
+}
+
+func leiaLinha() string {
+	return "esse texto que voce le agora e o texto informado pelo usuario"
+	// sc := bufio.NewScanner(os.Stdin)
+	// sc.Scan()
+	// return sc.Text()
 }
