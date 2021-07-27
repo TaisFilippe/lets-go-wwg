ed12c0305ef399befda85ae9bdbb4ddd487a17ac
Tais <taisfilippe@gmail.com>
2021-07-27 12:01:30 -0300

    add resolução

--- LaçosDeRepetição/Exercicio_04.go
+++ LaçosDeRepetição/Exercicio_04.go
@@ -1,7 +1,22 @@
 package main
 
+import (
+	"fmt"
+)
+
 // Faça um programa que solicite à usuária que informe um número até que o número informado seja par.
 
 func main() {
 
+	fmt.Println("Digite um número:")
+
+	var numero int
+	fmt.Scan(&numero)
+
+	for (numero % 2) != 0 {
+		fmt.Println("Digite um número:")
+		fmt.Scan(&numero)
+	}
+
+	fmt.Println("Finalmente seu tongo")
 }
